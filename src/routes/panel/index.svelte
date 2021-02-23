c20176b02785a007f970a79ed93cfd8b16656e91
Rafael <rafael.martinezcalvo@orange.fr>
2021-02-23 20:46:28 +0100

    added title and floating count

---
+++ src/routes/panel/index.svelte
@@ -0,0 +1,21 @@
+<h1>Select a BOT</h1>
+
+<div class="floating-count">2 / 3</div>
+
+<style>
+	h1 {
+		margin-top: 20px;
+		color: #adadad;
+		font-weight: bold;
+		font-size: 2.5em;
+	}
+	.floating-count {
+		position: fixed;
+		margin: 20px;
+		right: 0;
+		bottom: 0;
+		font-size: 2em;
+		font-weight: 900;
+		color: #adadad;
+	}
+</style>
