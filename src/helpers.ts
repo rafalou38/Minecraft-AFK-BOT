1fa242c3ad9da251593102fbf0d99f15d1203a79
Rafael <rafael.martinezcalvo@orange.fr>
2021-02-23 10:43:13 +0100

    added isBrowser in helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,5 +1,13 @@
+export function isBrowser() {
+	try {
+		return window != undefined;
+	} catch (e) {
+		return false;
+	}
+}
+
 export async function fetchJson(url: string) {
 	let r = await fetch(url);
 	let data = await r.json();
-	return data;
+	return data || {};
 }
