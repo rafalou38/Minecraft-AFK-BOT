01c203f4a47279deee6b86dabec66db2af047970
Rafael <rafael.martinezcalvo@orange.fr>
2021-03-02 18:16:06 +0100

    setup side bar to prevent change if not saved edits

--- src/components/panel/sidebar.svelte
+++ src/components/panel/sidebar.svelte
@@ -2,12 +2,24 @@
 	import Drawer, { Content, Header, Subtitle, Title } from "@smui/drawer";
 	import List, { Graphic, Item, Text } from "@smui/list";
 	import type { IBot } from "../../models/bot-model";
-	import { stores } from "@sapper/app";
+	import { stores, goto } from "@sapper/app";
 
 	const { page } = stores();
 	export let bot: IBot;
+	export let confirm: (objectif: string) => any;
+	async function handleClick(e: MouseEvent) {
+		const target = e.target as HTMLElement;
+		if (target.classList.contains("mdc-list-item")) {
+			const objectif = target.innerText
+				.split("\n")
+				.pop()
+				.toLocaleLowerCase();
 
-	console.log($page.path?.match("settings"));
+			if ((await confirm?.(objectif)) || !confirm) {
+				goto(`/panel/bot/${objectif}-${bot._id}`);
+			}
+		}
+	}
 </script>
 
 <Drawer variant="standard" open>
@@ -21,32 +33,26 @@
 	</Header>
 	<Content>
 		<List>
-			<Item
-				href={`panel/bot/status-${bot._id}`}
-				activated={$page.path?.match("status") !== null}
-			>
-				<Graphic class="material-icons" aria-hidden="true">info</Graphic
-				>
-				<Text>Status</Text>
-			</Item>
-			<Item
-				href={`panel/bot/settings-${bot._id}`}
-				activated={$page.path?.match("settings") !== null}
-			>
-				<Graphic class="material-icons" aria-hidden="true"
-					>settings</Graphic
-				>
-				<Text>Settings</Text>
-			</Item>
-			<Item
-				href={`panel/bot/actions-${bot._id}`}
-				activated={$page.path?.match("actions") !== null}
-			>
-				<Graphic class="material-icons" aria-hidden="true"
-					>build</Graphic
-				>
-				<Text>Actions</Text>
-			</Item>
+			<div on:click={handleClick}>
+				<Item activated={$page.path?.match("status") !== null}>
+					<Graphic class="material-icons" aria-hidden="true"
+						>info</Graphic
+					>
+					<Text>Status</Text>
+				</Item>
+				<Item activated={$page.path?.match("settings") !== null}>
+					<Graphic class="material-icons" aria-hidden="true"
+						>settings</Graphic
+					>
+					<Text>Settings</Text>
+				</Item>
+				<Item activated={$page.path?.match("actions") !== null}>
+					<Graphic class="material-icons" aria-hidden="true"
+						>build</Graphic
+					>
+					<Text>Actions</Text>
+				</Item>
+			</div>
 		</List>
 	</Content>
 </Drawer>
--- src/routes/panel/bot/settings-[bot_id].svelte
+++ src/routes/panel/bot/settings-[bot_id].svelte
@@ -11,19 +11,104 @@
 </script>
 
 <script lang="ts">
-	import type { IBot } from "../../../models/bot-model";
+	import Button, { Label } from "@smui/button";
+	import Textfield from "@smui/textfield";
+	import Snackbar, { Actions, Label as SnackbarLabel } from "@smui/snackbar";
+	import IconButton from "@smui/icon-button";
+
 	import Sidebar from "../../../components/panel/sidebar.svelte";
 
+	import type { IBot } from "../../../models/bot-model";
+	import { fetchJson } from "../../../helpers";
+
 	export let bot: IBot;
+	let initial_bot: any;
+	$: if (bot && !initial_bot) {
+		initial_bot = { ...bot };
+	}
+	async function confirmExit(objectif: string) {
+		if (
+			!(JSON.stringify({ ...initial_bot }) == JSON.stringify({ ...bot }))
+		) {
+			// TODO  Use smui dialog
+			return confirm("you didnt saved, are you sure? 😙");
+		}
+		return true;
+	}
+
+	let nothingToChangeSnackbar;
+	async function save() {
+		if (JSON.stringify({ ...initial_bot }) == JSON.stringify({ ...bot })) {
+			nothingToChangeSnackbar.open();
+		} else {
+			// TODO handle error
+			const response = await fetch(`panel/bot/`, {
+				method: "PUT",
+				body: JSON.stringify(bot),
+				headers: {
+					"Content-Type": "application/json",
+				},
+			});
+			if (response.status === 200) {
+				initial_bot = { ...bot };
+			}
+		}
+	}
 </script>
 
-<div class="panel">
-	<Sidebar {bot} />
+<Snackbar bind:this={nothingToChangeSnackbar}>
+	<SnackbarLabel>Nothing to save 🤨</SnackbarLabel>
+	<Actions>
+		<IconButton class="material-icons" title="Dismiss">close</IconButton>
+	</Actions>
+</Snackbar>
 
-	<h3>{bot}</h3>
+<div class="panel">
+	<Sidebar {bot} confirm={confirmExit} />
+
+	<div class="main">
+		<div class="form">
+			<h2>Settings</h2>
+			<Textfield
+				variant="outlined"
+				bind:value={bot.name}
+				label="Name"
+				style="width: 100%;"
+			/>
+
+			<Textfield
+				variant="outlined"
+				bind:value={bot.ip}
+				label="IP"
+				style="width: 100%;"
+			/>
+
+			<Textfield
+				variant="outlined"
+				bind:value={bot.username}
+				label="Username"
+				style="width: 100%;"
+			/>
+
+			<Textfield
+				variant="outlined"
+				bind:value={bot.username}
+				label="Password"
+				style="width: 100%;"
+			/>
+			<Button
+				variant="raised"
+				style="
+					width: 100%;
+					background-color: #028F01;
+				"
+				on:click={save}><Label>Save</Label></Button
+			>
+		</div>
+	</div>
 </div>
 
-<style>
+<style lang="scss">
 	.panel {
 		display: flex;
 		height: 100%;
@@ -31,5 +116,23 @@
 	}
 	:global(aside) {
 		height: auto !important;
+	}
+	.main {
+		width: 100%;
+	}
+	.form {
+		margin: 0 auto;
+		padding-top: 2em;
+
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+
+		gap: 1em;
+
+		width: 40%;
+		height: 100%;
+		min-width: 20em;
+		max-width: 30em;
 	}
 </style>
