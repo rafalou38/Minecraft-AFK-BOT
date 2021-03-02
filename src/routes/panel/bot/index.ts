063d99c9bd7276e63220d4ddb99f924e6722d8eb
Rafael <rafael.martinezcalvo@orange.fr>
2021-03-02 18:14:44 +0100

    create server route to edit existing bot

--- src/routes/panel/bot/index.ts
+++ src/routes/panel/bot/index.ts
@@ -1,5 +1,5 @@
 import type { Request, Response } from "express";
-import Bot from "../../../models/bot-model";
+import Bot, { IBot } from "../../../models/bot-model";
 
 export async function get(req: Request, res: Response, next: () => void) {
 	const results = await Bot.find({ owner: req.user }).lean().exec();
@@ -11,4 +11,34 @@ export async function get(req: Request, res: Response, next: () => void) {
 	res.json({
 		bots: results,
 	});
+}
+
+export async function put(req: Request, res: Response, next: () => void) {
+	const body = req.body as IBot;
+	if (body.name && body.ip && body.username && body._id) {
+		Bot.findByIdAndUpdate(
+			body._id,
+			{
+				name: body.name,
+				ip: body.ip,
+				username: body.username,
+			},
+			{ new: true }
+		)
+			.then((updatedBot) => {
+				if (updatedBot) {
+					res.statusCode = 200;
+					res.json(updatedBot);
+				} else {
+					res.statusCode = 404;
+					res.json(updatedBot);
+				}
+			})
+			.catch((err) => {
+				res.statusCode = 500;
+			});
+	} else {
+		res.statusCode = 422;
+		res.end();
+	}
 }
