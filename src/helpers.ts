f9b712b317ddd2f69449b672d94b91328d805c19
Rafael <rafael.martinezcalvo@orange.fr>
2021-03-13 10:36:42 +0100

    function to get unique id
    the id is based on the time wich makes it 100% unique

--- src/helpers.ts
+++ src/helpers.ts
@@ -11,3 +11,9 @@ export async function fetchJson(url: string) {
 	let data = await r.json();
 	return data || {};
 }
+
+export function uniqueID() {
+	return (
+		Date.now().toString(36) + Math.random().toString(36).substr(2, 5)
+	).toUpperCase();
+}
