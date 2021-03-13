2f5125f797f021f8ff6a527779658e6ee200f837
Rafael <rafael.martinezcalvo@orange.fr>
2021-03-13 10:36:02 +0100

    removed smui warning
    smui is bad :sob:

--- rollup.config.js
+++ rollup.config.js
@@ -21,6 +21,7 @@ const onwarn = (warning, onwarn) =>
 	(warning.code === "CIRCULAR_DEPENDENCY" &&
 		/[/\\]@sapper[/\\]/.test(warning.message)) ||
 	warning.code === "THIS_IS_UNDEFINED" ||
+	warning.filename?.match("node_modules\\\\@smui") ||
 	warning.message?.match("A11y") ||
 	onwarn(warning);
 
