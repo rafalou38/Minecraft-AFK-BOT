347f4e55bc1d2b3b45f38cfafc9c23ccd2f90628
Rafael <rafael.martinezcalvo@orange.fr>
2021-03-21 18:43:15 +0100

    verify if id is good before calling db

--- src/routes/panel/bot/action-[bot_id].ts
+++ src/routes/panel/bot/action-[bot_id].ts
@@ -2,11 +2,15 @@ import type { Request, Response } from "express";
 import Bot from "../../../models/bot-model";
 
 export async function get(req: Request, res: Response, next: () => void) {
-	console.log("call to db", req.params);
+	let { bot_id } = req.params;
+	if (bot_id.match(/^[0-9a-fA-F]{24}$/gm)) {
+		const bot = await Bot.findById(bot_id).lean().exec();
 
-	const bot = await Bot.findById(req.params.bot_id).lean().exec();
+		bot._id = bot._id.toString();
 
-	bot._id = bot._id.toString();
-
-	res.json(bot);
+		return res.json(bot);
+	} else {
+		res.status(422);
+		res.json({ error: "bad bot_id (does not match /^[0-9a-fA-F]{24}$/)" });
+	}
 }
